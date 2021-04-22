4aeb094b9ad201fd00779cac05cec70550ebd325
RicardoGFelix <jose.felix@ccc.ufcg.edu.br>
2021-04-22 15:46:27 -0300

    With RabbitMQ

--- src/controller/controller.js
+++ src/controller/controller.js
@@ -1,4 +1,7 @@
 const connection = require('../database/connections')
+const amqp = require('amqplib')
+const convert = require('json-2-csv')
+const FileSystem = require('fs')
 
 module.exports = {
     async createWorkflow(req, res){
@@ -17,6 +20,24 @@ module.exports = {
                 data,
                 steps
             }
+
+            const rabbitmq = async () => {
+                let conn = await amqp.connect('amqp://localhost:5672')
+                let ch = await conn.createChannel()
+
+                const msg = JSON.stringify(workflow)
+
+                ch.assertQueue('workflow_queue', {durable: false})
+                ch.sendToQueue('workflow_queue', new Buffer.from(msg))
+
+                console.log('Send %s', msg)
+
+                process.once('SIGINT', () => conn.close())
+
+            }
+
+            rabbitmq()
+            
             return res.json(workflow);
         } catch (error) {
             return res.json(error);
@@ -43,6 +64,55 @@ module.exports = {
             return res.json('The update was successful!');
         } catch (error) {
             return res.json(error);
+        }
+    },
+
+    // Método para que um workflow seja consumido
+    async consumeWorkflow(req, res){
+        try {
+            const workflow = await connection('workflow').select('*').where('status', 'inserted').first()
+
+            // retirar workflow consumido da fila
+            
+
+            let conn = await amqp.connect('amqp://localhost:5672')
+            let ch = await conn.createChannel()
+
+            const message = JSON.stringify(workflow)
+
+            ch.assertQueue('workflow_queue', {durable: false})
+            ch.prefetch(1)
+
+            console.log(' [x] Consuming message %s', message)
+
+            ch.consume('workflow_queue', msg => {
+                ch.ack(msg)
+                console.log('Message consumed!')
+                ch.close()
+                
+            }, { noAck: false })
+            
+            // alterar status do workflow consumido da fila para 'consumed' no banco de dados
+            
+            await connection('workflow').select('*').where('uuid', workflow.uuid).update('status', 'consumed')
+
+            
+            // gerando arquivo csv a partir dos dados em json do workflow consumido
+            
+            /*const data_workflow = [workflow.data]
+            
+            const csv = convert.json2csv(data_workflow, (error, csv) => {
+            if (error) { throw error }
+            
+            console.log(csv)
+            
+            FileSystem.writeFileSync('./csv-workflows/${workflow.uuid}.csv', csv)
+            
+            })*/
+
+            return res.json("Workflow consumido.")
+        } catch (error) {
+            return res.json(error)
         }
     }
 }
